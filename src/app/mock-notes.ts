b6ff4585cecc7117ef0fdab71c4530947ae4cb55
Andre Lomaka <andre7lomaka@gmail.com>
2021-05-06 21:33:26 +0300

    First commit

---
+++ src/app/mock-notes.ts
@@ -0,0 +1,14 @@
+import { Note } from './note';
+
+export const NOTES: Note[] = [
+  { title: 'Kassi ostunimekiri', text: 'Hiirele halvaad, hiirele martsipani, hiirele šokolaadi, oravale pähkleid' },
+  { title: 'Kassi 2. ostunimekiri', text: 'Piim (1.5L), jogurt (2x), müsli, leib, pikk sai, puuvilja (3), tomat, kurk, vürtsikilud, liha (2x), kohupiim (magus), keefir' },
+  { title: 'Title #3', text: 'Text Content #3' },
+  { title: 'Title #4', text: 'Text Content #4' },
+  { title: 'Title #5', text: 'Text Content #5' },
+  { title: 'Title #6', text: 'Text Content #6' },
+  { title: 'Title #7', text: 'Text Content #7' },
+  { title: 'Title #8', text: 'Text Content #8' },
+  { title: 'Title #9', text: 'Text Content #9' },
+  { title: 'Title #10', text: 'Text Content #10' }
+];
